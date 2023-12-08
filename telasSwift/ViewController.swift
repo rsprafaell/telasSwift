598ccb210b3e02592acd6d14b9a7dd62d47aef9a
rsprafaell <rsprafaell@hotmail.com>
2023-12-08 09:37:48 -0300

    Initial Commit

---
+++ telasSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  telasSwift
+//
+//  Created by IFB-BIOTIC-23 on 08/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
